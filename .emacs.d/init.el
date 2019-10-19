ec91303961f24977b88f97b42ce32fe1af98cf54
Oleg Grushenkov <ogrushenkov@gmail.com>
2019-10-18 23:07:27 -0400

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -631,7 +631,7 @@ START, END and COLUMN are the same as in `indent-region'."
  '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(fci-rule-color "#073642")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
